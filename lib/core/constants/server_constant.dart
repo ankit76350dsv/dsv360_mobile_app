1aeaf6ee06d92c01f1fe8ed17f47732773852c19
ANKIT KUMAR <ankit@dsvcorp.com.au>
2026-02-13 10:02:25 +0530

    I have added the production enviroment into code inot ios

--- lib/core/constants/server_constant.dart
+++ lib/core/constants/server_constant.dart
@@ -3,5 +3,5 @@ import 'package:dsv360/core/constants/environment.dart';
 class ServerConstant {
   static String serverURL = ENVIRONMENT.environment == "DEVELOPMENT"
       ? 'https://project-management-60040289923.development.catalystserverless.in/server/'
-      : 'https://project.dsv360.ai/server/';
+      : 'https://project-management-60040289923.catalystserverless.in/server/';
 }
